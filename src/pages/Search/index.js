d489ea61bf109b7b1c8cc9734074f51c2e10cdf6
Jeff-Enriquez <jeff.t.hawkes@gmail.com>
2020-05-30 16:29:34 -0700

    Add search page

---
+++ src/pages/Search/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import useStyles from './styles.js'
+
+const Search = () => {
+  const classes = useStyles()
+
+  return (
+    <h1>Search</h1>
+  )
+}
+
+export default Search
